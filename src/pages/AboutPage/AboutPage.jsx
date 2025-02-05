b901f22e658970d708ad833a7dfa09e1bfa3fe9e
Chantuka <47988187+Chantuu@users.noreply.github.com>
2025-02-06 03:39:30 +0400

    Add welcome animation in About Page

--- src/pages/AboutPage/AboutPage.jsx
+++ src/pages/AboutPage/AboutPage.jsx
@@ -1,15 +1,32 @@
 import React from 'react';
 import './AboutPage.css';
+import * as motion from 'motion/react-client';
 
 function AboutPage() {
     return (
         <main className="container">
-            <h1>About this project</h1>
-            <p>
+            <motion.h1
+                initial={{ opacity: 0, scale: 0 }}
+                animate={{ opacity: 1, scale: 1 }}
+                transition={{
+                    duration: 0.4,
+                    scale: { type: 'spring', visualDuration: 0.4, bounce: 0.5 },
+                }}
+            >
+                About this project
+            </motion.h1>
+            <motion.p
+                initial={{ opacity: 0, scale: 0 }}
+                animate={{ opacity: 1, scale: 1 }}
+                transition={{
+                    duration: 0.4,
+                    scale: { type: 'spring', visualDuration: 0.4, bounce: 0.5 },
+                }}
+            >
                 This project was made by Giorgi Chanturia for final React exams
                 in BTU university. Thank you very much for checking out this
                 project.
-            </p>
+            </motion.p>
         </main>
     );
 }
