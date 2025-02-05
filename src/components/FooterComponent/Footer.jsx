4afe84baf8623a6ff15ad0c71b3fd7d6d4ae00da
Chantuka <47988187+Chantuu@users.noreply.github.com>
2025-02-06 01:59:00 +0400

    Add Footer Component

---
+++ src/components/FooterComponent/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+    return (
+        <footer className="container">
+            <p>This project is made by Giorgi Chanturia.</p>
+        </footer>
+    );
+}
+
+export default Footer;
--- src/layouts/MainLayout/MainLayout.jsx
+++ src/layouts/MainLayout/MainLayout.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from '../../components/NavbarComponent/Navbar';
+import Footer from '../../components/FooterComponent/Footer';
 
 function MainLayout() {
     return (
         <>
             <Navbar />
             <Outlet />
+            <Footer />
         </>
     );
 }
