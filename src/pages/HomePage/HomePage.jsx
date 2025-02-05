c854623fed710f9cc0a12252d4734000085e2020
Chantuka <47988187+Chantuu@users.noreply.github.com>
2025-02-06 02:42:44 +0400

    Project CleanUp

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import MainSection from '../../components/MainSection/MainSection';
-import FashionLogoSection from '../../components/FashionLogoSection/FashionLogoSection';
+import MainSection from '../../components/MainSectionComponent/MainSection';
+import FashionLogoSection from '../../components/FashionLogoSectionComponent/FashionLogoSection';
 import DressStyleSection from '../../components/DressStyleSectionComponent/DressStyleSection';
 import ReviewsSection from '../../components/ReviewsSectionComponent/ReviewsSection';
 
