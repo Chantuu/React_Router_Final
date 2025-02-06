d9406d26ee66c2c9565b6f4722dca46dd4cc0776
Chantuka <47988187+Chantuu@users.noreply.github.com>
2025-02-06 15:27:29 +0400

    Add Section Content to New Sales Page

--- src/pages/NewSalesPage/NewSalesPage.jsx
+++ src/pages/NewSalesPage/NewSalesPage.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import './NewSalesPage.css';
 
 function NewSalesPage() {
-    return <div></div>;
+    return (
+        <section className="container clothingSection">
+            <h2>New Arrivals</h2>
+        </section>
+    );
 }
 
 export default NewSalesPage;
